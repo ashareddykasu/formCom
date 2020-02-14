c1ce543e270d2a6c2de1c397d058b1e077c6c669
ashareddykasu <ashareddyk16@gmail.com>
2020-02-14 08:04:24 +0530

    multiples of 3 and 5

---
+++ 02_fizzBuzz.js
@@ -0,0 +1,17 @@
+for (i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+      if (i % 5 == 0) {
+        console.log("FizzzBuzz");
+      } 
+      else console.log("Fizz");
+    }
+
+
+    else if (i % 5 == 0) {
+      console.log("Buzz");
+    } 
+    
+    else
+      console.log(i);
+  }
+  
